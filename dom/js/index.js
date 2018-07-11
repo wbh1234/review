f23532397cadf0a3ee0b418ef03f8f742f57015a
wbh1234 <18249214675@163.com>
2018-07-11 09:54:46 +0800

    the firstday review html and css review js review require css3 review this directer review requirejs and javascript review js fundation javascript typeof javascript operator Reference types object  array and RegExp function type arguments and this create an object inherit search dog

---
+++ dom/js/index.js
@@ -0,0 +1,75 @@
+/**
+ * Created by asus on 2018/7/10.
+ */
+
+
+var oOne= document.getElementById("one");
+var oTwo= document.getElementById("two");
+var oThree= document.getElementById("three");
+
+var leftSettingArrow = document.getElementById("leftSettingArrow");
+var leftLineArrow= document.getElementById("leftLineArrow");
+var oSettingList = document.getElementById("settingList");
+var oLeftLineList = document.getElementById("leftLineList");
+
+oOne.onmouseover = function(){
+    oOne.style.color="green";
+    oOne.style.borderLeft="solid 2px green";
+};
+oOne.onmouseout = function(){
+    oOne.style.color="black";
+    oOne.style.borderLeft="none";
+
+};
+oTwo.onmouseover = function(){
+    oTwo.style.color="green";
+    oTwo.style.borderLeft="solid 2px green";
+};
+oTwo.onmouseout = function(){
+    oTwo.style.color="black";
+    oTwo.style.borderLeft="none";
+
+};
+oThree.onmouseover = function(){
+    oThree.style.color="green";
+    oThree.style.borderLeft="solid 2px green";
+};
+oThree.onmouseout = function(){
+    oThree.style.color="black";
+    oThree.style.borderLeft="none";
+
+};
+
+
+leftSettingArrow.onclick=function () {
+
+
+    var val = oSettingList.style.display;
+    if(val == 'none'){
+        oSettingList.style.display = 'block';
+    }else{
+        oSettingList.style.display = 'none';
+    }
+
+};
+leftLineArrow.onclick=function(){
+    var val1 =oLeftLineList.style.display;
+    if(val1 == 'none'){
+        oLeftLineList.style.display = 'block';
+    }else{
+        oLeftLineList.style.display = 'none';
+    }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
